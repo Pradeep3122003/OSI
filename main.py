2c18352d4ed8b43a116c005aa51d854f2542ea01
Pradeep3122003 <pradeepkh312@gmail.com>
2025-02-01 17:12:31 +0530

    update main file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import subprocess
 
 def run_layer(file):
     print(f"Running {file}...")
-    subprocess.run(["python3", file])
+    subprocess.run(["python", file])
 
 if __name__ == "__main__":
     layers = [
